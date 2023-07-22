25ac604cf2722913d82bf17f93c135f0593d8485
mohansingh509 <70848477+mohansingh509@users.noreply.github.com>
2023-07-22 16:37:46 +0530

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,33 @@
-const loginController=(req,res)=>{
-    res.render('login');
-    // res.redirect('index');
+import { FrappeApp } from 'frappe-js-sdk';
+const frappe = new FrappeApp('http://20.163.238.153');
+
+
+ const login= async (req,res)=>{ 
+    console.log(req.body)
+const data = {
+    username: req.body.username,
+    password: req.body.password
+  }
+  
+  const auth = await frappe.auth();
+    auth.loginWithUsernamePassword(data)
+  
+    .then((response) => {
+        console.log('Logged in')
+       return res.redirect('/')
+       
+ })
+    .catch((error) => console.error(error));
 }
 
-export { loginController }
+const loginController=(req,res)=>{
+  
+   
+        
+        
+ 
+    
+    res.render('login',)
+}
+
+export { loginController ,login}
